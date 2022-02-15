3b1c00d6e3796045e405cfaeccc615bca21e5a13
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-15 17:08:43 +0500

    Question4.Area of Triangle

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -17,12 +17,19 @@
 
 //Question#3
 // // Current Date in javaScript
-var today = new Date();
-var date = today.getMonth() + '-' + (today.getDate() + 1) + '-' + today.getFullYear();
-document.getElementById("Current-Date").innerHTML = date;
+// var today = new Date();
+// var date = today.getMonth() + '-' + (today.getDate() + 1) + '-' + today.getFullYear();
+// document.getElementById("Current-Date").innerHTML = date;
 
+// var today = new Date();
+// var date = today.getMonth() + '/' + (today.getDate() + 1) + '/' + today.getFullYear();
+// document.getElementById("Current/Date").innerHTML = date;
 
-var today = new Date();
-// var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-var date = today.getMonth() + '/' + (today.getDate() + 1) + '/' + today.getFullYear();
-document.getElementById("Current/Date").innerHTML = date;
+var side1 = 5;
+var side2 = 6;
+var side3 = 7;
+
+var TotalSide = side1 + side2 + side3;
+var s = TotalSide / 2;
+var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+document.writeln('Area=' + area);
