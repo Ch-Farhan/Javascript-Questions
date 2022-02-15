5ff60f53b22e067457fd22c178ba73b3007c4af8
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-15 20:20:42 +0500

    Calculate Days left until next Christmas

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -100,17 +100,54 @@
 //     }
 // }
 
-//Question#7
-function guess() {
-    var num = parseInt(Math.random() * 10 + 1);
-    var userGuess = prompt("Input a guess number between 1 to 10");
-    if (isNaN(userGuess)) {
-        alert("Please, write a number");
-    } else {
-        if (num == userGuess) {
-            alert("Wow you guessed it :D");
-        } else {
-            alert("Not matched. Number was " + num);
-        }
-    }
+//Question#8
+// function guess() {
+//     var num = parseInt(Math.random() * 10 + 1);
+//     var userGuess = prompt("Input a guess number between 1 to 10");
+//     if (isNaN(userGuess)) {
+//         alert("Please, write a number");
+//     } else {
+//         if (num == userGuess) {
+//             alert("Wow you guessed it :D");
+//         } else {
+//             alert("Not matched. Number was " + num);
+//         }
+//     }
+// }
+
+// Question9
+// Get the current date
+let today = new Date();
+
+// Get the year of the current date
+let christmasYear = today.getFullYear();
+
+// Check if the current date is
+// already past by checking if the month
+// is December and the current day
+// is greater than 25
+if (today.getMonth() == 11 &&
+    today.getDate() > 25) {
+
+    // Add an year so that the next
+    // Christmas date could be used
+    christmasYear = christmasYear + 1;
 }
+
+// Get the date of the next Christmas
+let christmasDate =
+    new Date(christmasYear, 11, 25);
+
+// Get the number of milliseconds in 1 day
+let dayMilliseconds =
+    1000 * 60 * 60 * 24;
+
+// Get the remaining amount of days
+let remainingDays = Math.ceil(
+    (christmasDate.getTime() - today.getTime()) /
+    (dayMilliseconds)
+);
+
+// Write it to the page
+document.write("There are " + remainingDays +
+    " days remaining until Christmas.");
