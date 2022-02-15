7c00df5c2c6b4126b1e01d88a3a4352b7469742b
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-15 17:29:19 +0500

    Rotate Direction of w3resource

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -25,11 +25,26 @@
 // var date = today.getMonth() + '/' + (today.getDate() + 1) + '/' + today.getFullYear();
 // document.getElementById("Current/Date").innerHTML = date;
 
-var side1 = 5;
-var side2 = 6;
-var side3 = 7;
+//Question#4
+// var side1 = 5;
+// var side2 = 6;
+// var side3 = 7;
 
-var TotalSide = side1 + side2 + side3;
-var s = TotalSide / 2;
-var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
-document.writeln('Area=' + area);
+// var TotalSide = side1 + side2 + side3;
+// var s = TotalSide / 2;
+// var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+// document.writeln('Area=' + area);
+
+//Question#5
+var myVar = setInterval(myFunction, 1000);
+
+var word = 'w3resource';
+var newArray = word.split('');
+
+function myFunction() {
+    var letter = newArray.pop();
+    var newWord = newArray.unshift(letter);
+    var theWord = newArray.join('');
+    console.log(theWord);
+    document.getElementById('rotate').innerHTML = theWord;
+}
