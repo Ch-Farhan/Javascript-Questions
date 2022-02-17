46cf2ff295d0fa150d1068e301d16143ae201d52
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-17 13:12:40 +0500

    Q#23. Write a JavaScript program to create a new string from a given string changing the position of first and last characters. The string length must be greater than or equal to 1. Completed

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -280,9 +280,21 @@
 // console.log(string_check("thon"));
 
 // Question22
-function removeCharacter() {
-    originalString = 'qNARinternational';
-    newString = originalString.replace('q', '');
+// function removeCharacter() {
+//     originalString = 'qNARinternational';
+//     newString = originalString.replace('q', '');
 
-    document.querySelector('.output').textContent = newString;
+//     document.querySelector('.output').textContent = newString;
+// }
+
+// Question23
+function first_last(str1) {
+    if (str1.length <= 1) {
+        return str1;
+    }
+    mid_char = str1.substring(1, str1.length - 1);
+    return (str1.charAt(str1.length - 1)) + mid_char + str1.charAt(0);
 }
+console.log(first_last('a'));
+console.log(first_last('ab'));
+console.log(first_last('abc'));
