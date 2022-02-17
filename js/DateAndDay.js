74056b2fb621ef25598e196cd8002455ad0aed7c
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-17 17:39:00 +0500

    Q#31. Write a JavaScript program to find the largest of three given integers. Completed

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -353,15 +353,35 @@
 // console.log(three(65, 9, 199));
 
 // Question30
-function check_script(str) {
-    if (str.length < 6) {
-        return str;
-    }
-    let result_str = str;
-    if (str.substring(10, 4) == 'Script') {
-        result_str = str.substring(0, 4) + str.substring(10, str.length);
-    }
-    return result_str;
+// function check_script(str) {
+//     if (str.length < 6) {
+//         return str;
+//     }
+//     let result_str = str;
+//     if (str.substring(10, 4) == 'Script') {
+//         result_str = str.substring(0, 4) + str.substring(10, str.length);
+//     }
+//     return result_str;
+// }
+// console.log(check_script("JavaScript"));
+// console.log(check_script("CoffeeScript"));
+
+// Question31
+// program to find the largest among three numbers
+
+// take input from the user
+const num1 = parseFloat(prompt("Enter first number: "));
+const num2 = parseFloat(prompt("Enter second number: "));
+const num3 = parseFloat(prompt("Enter third number: "));
+let largest;
+
+// check the condition
+if (num1 >= num2 && num1 >= num3) {
+    largest = num1;
+} else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+} else {
+    largest = num3;
 }
-console.log(check_script("JavaScript"));
-console.log(check_script("CoffeeScript"));
+// display the result
+console.log("The largest number is " + largest);
