4386a0305e9595a6546c84af186b00074d925aac
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-17 17:47:26 +0500

    Q#32. Write a JavaScript program to find a value which is nearest to 100 from two different given integer values.  Completed

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -370,18 +370,30 @@
 // program to find the largest among three numbers
 
 // take input from the user
-const num1 = parseFloat(prompt("Enter first number: "));
-const num2 = parseFloat(prompt("Enter second number: "));
-const num3 = parseFloat(prompt("Enter third number: "));
-let largest;
+// const num1 = parseFloat(prompt("Enter first number: "));
+// const num2 = parseFloat(prompt("Enter second number: "));
+// const num3 = parseFloat(prompt("Enter third number: "));
+// let largest;
 
-// check the condition
-if (num1 >= num2 && num1 >= num3) {
-    largest = num1;
-} else if (num2 >= num1 && num2 >= num3) {
-    largest = num2;
-} else {
-    largest = num3;
+// // check the condition
+// if (num1 >= num2 && num1 >= num3) {
+//     largest = num1;
+// } else if (num2 >= num1 && num2 >= num3) {
+//     largest = num2;
+// } else {
+//     largest = num3;
+// }
+// // display the result
+// console.log("The largest number is " + largest);
+
+// Question32
+function nearest(a, b) {
+    var a1 = 100 - a
+    var b1 = 100 - b
+    if (a1 > b1) {
+        return b + " is nearest"
+    } else {
+        return a + " is nearest"
+    }
 }
-// display the result
-console.log("The largest number is " + largest);
+console.log(nearest(10, 9))
