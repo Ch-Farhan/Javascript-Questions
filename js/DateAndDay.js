8d54ef31407fbf7194f03ae21f9c183e9de9852b
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-18 12:20:30 +0500

    Q#37. Write a JavaScript program to create new string with first 3 characters are in lower case from a given string. If the string length is less than 3 convert all the characters in upper case. Completed

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -453,18 +453,29 @@
 numbers is same */
 
 // take input
-const a = prompt('Enter a first integer: ');
-const b = prompt('Enter a second integer: ');
-const c = prompt('Enter a third integer: ');
+// const a = prompt('Enter a first integer: ');
+// const b = prompt('Enter a second integer: ');
+// const c = prompt('Enter a third integer: ');
 
-// find the last digit
-const result1 = a % 10;
-const result2 = b % 10;
-const result3 = c % 10;
+// // find the last digit
+// const result1 = a % 10;
+// const result2 = b % 10;
+// const result3 = c % 10;
 
-// compare the last digits
-if (result1 == result2 && result1 == result3) {
-    console.log(`${a}, ${b} and ${c} have the same last digit.`);
-} else {
-    console.log(`${a}, ${b} and ${c} have different last digit.`);
+// // compare the last digits
+// if (result1 == result2 && result1 == result3) {
+//     console.log(`${a}, ${b} and ${c} have the same last digit.`);
+// } else {
+//     console.log(`${a}, ${b} and ${c} have different last digit.`);
+// }
+
+// Question37
+function newstring(n) {
+    console.log(n)
+    if (n.length > 3) {
+        return n.substring(0, 3).toUpperCase() + n.substring(3, n.length)
+    } else {
+        return n.toUpperCase()
+    }
 }
+console.log(newstring("jayant"))
