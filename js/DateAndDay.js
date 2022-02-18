94360b2f50a62a627b41f0dc25c775dc3338d8b1
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-18 16:15:31 +0500

    Q#61. Write a JavaScript program to concatenate two strings except their first character. Completed

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -737,10 +737,20 @@ numbers is same */
 // console.log(first_half("JavaScript"));
 // console.log(first_half("PHP"));
 
-// Question59
-function without_first_end(str) {
-    return str.substring(1, str.length - 1);
+// Question60
+// function without_first_end(str) {
+//     return str.substring(1, str.length - 1);
+// }
+// console.log(without_first_end('JavaScript'));
+// console.log(without_first_end('JS'));
+// console.log(without_first_end('PHP'));
+
+// Question61
+function concatenate(str1, str2) {
+    str1 = str1.substring(1, str1.length);
+    str2 = str2.substring(1, str2.length);
+    return str1 + str2;
 }
-console.log(without_first_end('JavaScript'));
-console.log(without_first_end('JS'));
-console.log(without_first_end('PHP'));
+console.log(concatenate("PHP", "JS"));
+console.log(concatenate("A", "B"));
+console.log(concatenate("AA", "BB"));
