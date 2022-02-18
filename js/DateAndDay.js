fe1c8aef809ea85ff6c5d42ff3d1d502b73729ca
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-18 15:53:03 +0500

    Q#56. Write a JavaScript program to divide two positive numbers and return a string with properly formatted commas. Completed

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -680,12 +680,25 @@ numbers is same */
 // console.log(result);
 
 // Question55
-function equal_pt(str) {
-    var str_p = str.replace(/[^p]/g, "");
-    var str_t = str.replace(/[^t]/g, "");
-    var p_num = str_p.length;
-    var s_num = str_t.length;
-    return p_num === s_num;
+// function equal_pt(str) {
+//     var str_p = str.replace(/[^p]/g, "");
+//     var str_t = str.replace(/[^t]/g, "");
+//     var p_num = str_p.length;
+//     var s_num = str_t.length;
+//     return p_num === s_num;
+// }
+// console.log(equal_pt("paatpss"));
+// console.log(equal_pt("paatps"));
+
+// Question56
+n1 = 80;
+n2 = 6;
+var div = Math.round(n1 / n2).toString(),
+    result_array = div.split("");
+if (div >= 1000) {
+    for (var i = div.length - 3; i > 0; i -= 3) {
+        result_array.splice(i, 0, ",");
+    }
+    result_array;
 }
-console.log(equal_pt("paatpss"));
-console.log(equal_pt("paatps"));
+console.log(result_array);
