958b9de87196b506db90b4605ae48dd82da00585
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-21 17:23:06 +0500

    Q#9. Chunk. Write a method that splits an array into parts of determined size. Completed

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -903,15 +903,33 @@ numbers is same */
 //     document.write("False");
 
 // Q#8.
-var arrays = [
-    ["$6"],
-    ["$12"],
-    ["$25"],
-    ["$25"],
-    ["$18"],
-    ["$22"],
-    ["$10"]
-];
-var merged = [].concat.apply([], arrays);
+// var arrays = [
+//     ["$6"],
+//     ["$12"],
+//     ["$25"],
+//     ["$25"],
+//     ["$18"],
+//     ["$22"],
+//     ["$10"]
+// ];
+// var merged = [].concat.apply([], arrays);
 
-console.log(merged);
+// console.log(merged);
+
+// Q#9.
+// program to split array into smaller chunks
+
+function splitIntoChunk(arr, chunk) {
+
+    for (i = 0; i < arr.length; i += chunk) {
+
+        let tempArray;
+        tempArray = arr.slice(i, i + chunk);
+        console.log(tempArray);
+    }
+
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+const chunk = 2;
+splitIntoChunk(array, chunk);
