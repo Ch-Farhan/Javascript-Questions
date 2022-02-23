4bb471c102ecab937e29dc69321687aa6d2c6bf6
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-23 13:16:04 +0500

    Q#5. write a function that returns a Boolean indicating whether the given string contains a valid nesting of braces.  Completed

--- js/DateAndDay.js
+++ js/DateAndDay.js
@@ -1190,26 +1190,77 @@ anagram of each other */
 //         " anagram of each other<br>");
 
 // Q#4.
-const str = 'Farhan';
-const canFormPalindrome = (str = '') => {
-    const hash = {};
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        let c = str[i];
-        if (c === ' ') {
+// const str = 'Farhan';
+// const canFormPalindrome = (str = '') => {
+//     const hash = {};
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         let c = str[i];
+//         if (c === ' ') {
+//             continue;
+//         };
+//         if (hash[c]) {
+//             delete hash[c];
+//         } else {
+//             hash[c] = true;
+//         };
+//         count++;
+//     };
+//     if (count % 2 === 0) {
+//         return Object.keys(hash).length === 0;
+//     } else {
+//         return Object.keys(hash).length === 1;
+//     };
+// };
+// console.log(canFormPalindrome(str));
+
+// Q#5.
+// Javascript program for checking
+// balanced brackets
+// Function to check if brackets are balanced
+function areBracketsBalanced(expr) {
+    // Using ArrayDeque is faster
+    // than using Stack class
+    let stack = [];
+    // Traversing the Expression
+    for (let i = 0; i < expr.length; i++) {
+        let x = expr[i];
+        if (x == '(' || x == '[' || x == '{') {
+            // Push the element in the stack
+            stack.push(x);
             continue;
-        };
-        if (hash[c]) {
-            delete hash[c];
-        } else {
-            hash[c] = true;
-        };
-        count++;
-    };
-    if (count % 2 === 0) {
-        return Object.keys(hash).length === 0;
-    } else {
-        return Object.keys(hash).length === 1;
-    };
-};
-console.log(canFormPalindrome(str));
+        }
+        // If current character is not opening
+        // bracket, then it must be closing.
+        // So stack cannot be empty at this point.
+        if (stack.length == 0)
+            return false;
+        let check;
+        switch (x) {
+            case ')':
+                check = stack.pop();
+                if (check == '{' || check == '[')
+                    return false;
+                break;
+            case '}':
+                check = stack.pop();
+                if (check == '(' || check == '[')
+                    return false;
+                break;
+            case ']':
+                check = stack.pop();
+                if (check == '(' || check == '{')
+                    return false;
+                break;
+        }
+    }
+    // Check Empty Stack
+    return (stack.length == 0);
+}
+// Driver code
+let expr = "([{}])";
+// Function call
+if (areBracketsBalanced(expr))
+    document.write("Balanced ");
+else
+    document.write("Not Balanced ");
